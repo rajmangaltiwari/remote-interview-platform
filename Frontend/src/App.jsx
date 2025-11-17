5849cbd39b0cc57524d34ed879e771dba5b354c2
Rajmangal Tiwari <rajmangltiwari@gmail.com>
2025-11-17 11:33:54 +0530

    homepage added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton, useUser } from '@clerk/clerk-react'
 import { Navigate, Route, Routes } from 'react-router'
+import { Toaster} from 'react-hot-toast'
+
 import Homepage from './pages/Homepage'
 import Problempage from './pages/Problempage'
-import { Toaster} from 'react-hot-toast'
+import Dashboard from './pages/Dashboard'
 
 function App() {
 
-  const { isSignedIn } = useUser()
+  const { isSignedIn,isLoaded } = useUser()
+
+  if(!isLoaded) return null;
   return (
     <>
       <Routes>
         <Route path='/' element={<Homepage />} />
+        <Route path='/dashboard' element={isSignedIn ? <Dashboard /> : <Navigate to='/' />} />
         <Route path='/problems' element={isSignedIn ? <Problempage /> : <Navigate to='/' />} />
       </Routes>
     <Toaster />
